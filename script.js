ec5a1f8d96201354e2928b3f921ab4f296693908
Ayush01arya <69747175+Ayush01arya@users.noreply.github.com>
2020-08-17 12:28:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,250 @@
+var _smartsupp = _smartsupp || {};
+_smartsupp.key =
+ 'eef966bdcc05ba53b77283202970fc27afc1f145';
+window.smartsupp || (function(d) {
+ var s, c, o = smartsupp =
+  function() {
+   o._.push(arguments)
+  };
+ o._ = [];
+ s = d.getElementsByTagName(
+  'script')[0];
+ c = d.createElement('script');
+ c.type = 'text/javascript';
+ c.charset = 'utf-8';
+ c.async = true;
+ c.src =
+  'https://www.smartsuppchat.com/loader.js?';
+ s.parentNode.insertBefore(c, s);
+})(document);
+
+function generateOTP() {
+
+
+
+ // Declare a digits variable  
+
+ // which stores all digits 
+
+ var digits = '0123456789a';
+
+ let OTP = '';
+
+ for (let i = 0; i < 6; i++) {
+
+  OTP += digits[Math.floor(Math
+   .random() * 10)];
+
+ }
+
+ return OTP;
+
+}
+
+
+
+document.write("YOUR PASSWORD :👉 ")
+
+var area = generateOTP()
+document.write(area);
+
+
+function googleTranslateElementInit() {
+
+ new google.translate
+  .TranslateElement({
+
+   pageLanguage: 'en'
+
+  }, 'google_translate_element');
+
+}
+
+
+
+
+function validate() {
+ var email = document
+  .getElementById("email").value;
+ var password = document
+  .getElementById("password").value;
+ if (email ==
+  "ayusharya11oct@gmail.com" &&
+  password == area) {
+
+  window.location =
+   "https://757q02.mimo.run/saathi.html?";
+  return false;
+ }
+ if (email == "aliza2020@gmail.com" &&
+  password == area) {
+  window.location =
+   "https://757q02.mimo.run/saathi.html?";
+  return false;
+ }
+ if (email ==
+  "kaushtubhsinghmzp@gmail.com" &&
+  password == area) {
+  window.location =
+   "https://757q02.mimo.run/saathi.html?";
+  return false;
+ }
+ if (email ==
+  "Saurabh19842006@gmail.com" &&
+  password == area) {
+  window.location =
+   "https://757q02.mimo.run/saathi.html?";
+  return false;
+ }
+ if (email ==
+  "amitsir2020@gmail.com" &&
+  password == area) {
+  window.location =
+   "https://757q02.mimo.run/saathi.html?";
+  return false;
+ }
+ if (email ==
+  "25aditiagrawal@gmail.com" &&
+  password == area) {
+  window.location =
+   "https://757q02.mimo.run/saathi.html?";
+  return false;
+ }
+
+ if (email ==
+  "prabhatyadavmzp2003@gmail.com" &&
+  password ==
+  area) {
+  window.location =
+   "https://757q02.mimo.run/saathi.html?";
+  return false;
+ }
+ if (email == "" && password == "") {
+  alert(
+   "Error: Username cannot be blank!");
+
+  return false;
+ }
+ if (email ==
+  "mohammmadsaifali@gmail.com" &&
+  password ==
+  area) {
+  window.location =
+   "https://757q02.mimo.run/saathi.html?";
+  return false;
+ }
+ if (email ==
+  "ukt2904@gmail.com" &&
+  password ==
+  area) {
+  window.location =
+   "https://757q02.mimo.run/saathi.html?";
+  return false;
+ }
+ if (email ==
+  "amankeshari3830@gmail.com" &&
+  password ==
+  area) {
+  window.location =
+   "https://757q02.mimo.run/saathi.html?";
+  return false;
+ }
+ if (email == "Tabrez" || email ==
+  "tabrez" && password == area) {
+  window.location =
+   "https://757q02.mimo.run/saathi.html?";
+  return false;
+ }
+ if (email ==
+  "yogi.98890@gmail.com" &&
+  password ==
+  area) {
+  window.location =
+   "https://757q02.mimo.run/saathi.html?";
+  return false;
+ }
+ if (email ==
+  "ayusharya11oct@gmail.com" &&
+  password == "223566") {
+
+  window.location =
+   "https://xb6ur2.mimo.run/index.html?";
+  return false;
+ }
+ if (email == "aliza2020@gmail.com" &&
+  password == "223566") {
+  window.location =
+   "https://jecfrq.mimo.run/index.html?";
+  return false;
+ }
+ if (email ==
+  "Saurabh19842006@gmail.com" &&
+  password == "223566") {
+  window.location =
+   "https://suduwz.mimo.run/index.html?";
+  return false;
+ }
+ if (email ==
+  "amitsir2020@gmail.com" &&
+  password == "223566") {
+  window.location =
+   "https://vl1mnx.mimo.run/index.html?";
+  return false;
+ }
+ if (email ==
+  "25aditiagrawal@gmail.com" &&
+  password == "223566") {
+  window.location =
+   "https://yyul5j.mimo.run/index.html?";
+  return false;
+ }
+ if (email ==
+  "prabhatyadavmzp2003@gmail.com" &&
+  password ==
+  "223566") {
+  window.location =
+   "https://58okbm.mimo.run/index.html?";
+  return false;
+ }
+ if (email == "" && password == "") {
+  alert(
+   "Error: Username cannot be blank!");
+
+  return false;
+ }
+ if (email ==
+  "mohammmadsaifali@gmail.com" &&
+  password ==
+  "223566") {
+  window.location =
+   "https://0e4px1.mimo.run/index.html?";
+  return false;
+ }
+ if (email ==
+  "ukt2904@gmail.com" &&
+  password ==
+  "223566") {
+  window.location =
+   "https://7i1ndo.mimo.run/index.html?";
+  return false;
+ }
+ if (email ==
+  "amankeshari3830@gmail.com" &&
+  password ==
+  "223566") {
+  window.location =
+   "https://cbn5hg.mimo.run/index.html?";
+  return false;
+ }
+ if (email ==
+  "yogi.98890@gmail.com" &&
+  password ==
+  "223566") {
+  window.location =
+   "https://lfzmza.mimo.run/index.html?";
+  return false;
+ } else {
+  window.location =
+   "https://aoe0wx.mimo.run/index.html?";
+ }
+}
